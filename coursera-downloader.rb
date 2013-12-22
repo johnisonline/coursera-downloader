99bceaa5eba94f33288c260c72f2342aef131d9c
Justin W. Smith <justin.w.smith@gmail.com>
2013-12-21 21:23:19 -0500

    Multiple Fixes & Refactorings

--- coursera-downloader.rb
+++ coursera-downloader.rb
@@ -1,94 +1,145 @@
 require "mechanize"
 require "mechanize/http/content_disposition_parser"
 require "uri"
-require 'net/http'
+require 'net/https'
 require 'cgi'
 require 'nokogiri'
 
+class CourseraSession
 
-if ARGV.size < 3
+  @@base_course_uri = 'https://class.coursera.org/%s'
+  @@login_uri = 'https://accounts.coursera.org/api/v1/login'
+  @@course_content_path = '/lecture/index'
+
+  def CourseraSession.parse_value(key, string)
+    regexp = Regexp.new("#{key}=([^;]+)")
+    string.match(/CAUTH=([^;]+)/)[1]
+  end
+
+  def initialize username, password, course_name
+    @username = username
+    @password = password
+    @course_name = course_name
+  end
+
+  def course_uri
+    @course_uri ||= URI(@@base_course_uri % @course_name)
+  end
+
+  def course_content_uri
+    @course_content_uri ||= URI((@@base_course_uri % @course_name) + @@course_content_path)
+  end
+
+
+  def csrf_token
+    unless @csrf_token
+      http = Net::HTTP.new(course_uri.host, course_uri.port)
+      http.use_ssl = (course_uri.scheme == 'https')
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      
+      response = http.get(course_uri.path)
+      raise "Unable to connect to course" unless response["Set-Cookie"]
+      @csrf_token = response["Set-Cookie"].split(";")[0].split("=")[1]
+    end
+    @csrf_token
+  end
+
+  def cauth
+    unless @cauth
+      uri = URI(@@login_uri)
+
+      Net::HTTP.start(uri.host, uri.port, 
+                      :use_ssl => (uri.scheme == 'https'), 
+                      :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+
+        request = Net::HTTP::Post.new( uri.path,
+                                       'Cookie' =>  "csrftoken=#{csrf_token}",
+                                       'X-CSRFToken' => csrf_token,
+                                       'Referer' => 'https://accounts.coursera.org/signin'
+                                        )
+        request.set_form_data('email' => @username, 'password' => @password)
+
+        response = http.request(request)
+        raise "Unable to complete login." unless response["Set-Cookie"]
+        cookies = response["Set-Cookie"]
+
+        @cauth = CourseraSession.parse_value('CAUTH', cookies)
+        @username = nil
+        @password = nil
+      end
+    end
+    @cauth
+  end
+
+  def cookies
+    unless @cookies
+      @cookies = { maestro_login_flag: 1, CAUTH: cauth }
+    end
+    @cookies
+  end
+
+  def cookie_string
+    unless @cookie_string
+      result = ''
+      cookies.each do |key, val|
+        result += "#{key}=#{val};"
+      end
+      @cookie_string = result[0...-1]
+    end
+    @cookie_string
+  end
+
+  def course_content
+    #not cached
+    uri = URI( course_content_uri )
+    Net::HTTP.start(uri.host, uri.port, 
+                    :use_ssl => (uri.scheme == 'https'),
+                    :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+      request = Net::HTTP::Get.new(
+        uri.path,
+        'Cookie' => cookie_string)
+
+      response = http.request(request)
+      raise "Unable to load course content" unless response.body
+      response.body
+    end
+  end
+
+  def get_resource_links
+    #not cached
+    page = Nokogiri::HTML(course_content)
+    links = []
+
+    page.css('div.course-lecture-item-resource').each do |div|
+       div.css('a').each do |link|
+        links << link.attributes['href'].value
+       end
+    end
+    links
+  end
+end
+
+
+
+if ARGV.size != 3
   puts "coursera-downloader.rb <username> <password> <course>"
   exit 1
 end
 
-def course_uri(course_name)
-  URI("https://class.coursera.org/#{course_name}")
-end
-
-def initial_response(uri)
-  Net::HTTP.get_response(uri)
-end
-
-def do_login(initial_response, username, password)
-  uri = URI('https://class.coursera.org/progfun-2012-001')
-  cookies = ""
-
-  Net::HTTP.start(uri.host, uri.port,:use_ssl => uri.scheme == 'https') do |http|
-    request = Net::HTTP::Post.new(uri)
-    request['Cookie'] = "csrftoken=#{initial_response["Set-Cookie"].split(";")[0].split("=")[1]}"
-    request["X-CSRFToken"] = initial_response["Set-Cookie"].split(";")[0].split("=")[1]
-    request["Referer"] = "https://accounts.coursera.org/signin"
-
-    request.set_form_data('email' => username, 'password' => password)
-
-    response = http.request(request)
-    cookies = response["set-cookie"]
-  end
-
-  return cookies
-end
-
-def build_cookie_string(cookies)
-  cauth = cookies["CAUTH"]
-  return "maestro_login_flag=1;CAUTH=#{cauth}"
-end
-
-def course_content_uri(course_name)
- URI("https://class.coursera.org/#{course_name}/lecture/index")
-end
-
-
-def get_content_site(cookie_string, course_name)
-  data = ""
-  uri = course_content_uri(course_name)
-  Net::HTTP.start(uri.host, uri.port,:use_ssl => uri.scheme == 'https') do |http|
-    request = Net::HTTP::Get.new(uri)
-    request["Cookie"] = cookie_string
-
-    data = http.request(request).body
-  end
-
-  return data
-end
-
-def get_download_links(data)
-  page = Nokogiri::HTML(data)
-  result = []
-
-  page.css('div.course-lecture-item-resource').each do |div|
-     div.css('a').each do |link|
-      result << link.attributes['href'].value
-     end
-  end
-
-  return result
-end
-
-username = ARGV[0]
-password = ARGV[1]
-course_name = ARGV[2]
-
-initial_resp = initial_response(course_uri(course_name))
-cookies = do_login(initial_resp, username, password)
-cookie_string = build_cookie_string(cookies)
-
-data = get_content_site(cookie_string, course_name)
-links = get_download_links(data)
+session = CourseraSession.new *ARGV
 
 agent = Mechanize.new
+
+session.cookies.each do |key, val|
+  cookie = Mechanize::Cookie.new(key.to_s, val.to_s)
+  cookie.domain = "class.coursera.org"
+  cookie.path = "/"
+  agent.cookie_jar.add(cookie)
+end
+
 # Download all files to the current directory
-links.each do |link|
-  unless (link=~ URI::regexp).nil?
+session.get_resource_links.each do |link|
+  unless (link =~ URI::regexp).nil?
     uri = link
     if (uri =~ /\.mp4/) || (uri =~ /srt/) || (uri =~ /\.pdf/) || (uri =~ /\.pptx/)
       begin
@@ -96,6 +147,9 @@ links.each do |link|
       rescue Mechanize::ResponseCodeError => exception
         if exception.response_code == '403'
           filename = URI.decode(exception.page.filename).gsub(/.*filename=\"(.*)\"+?.*/, '\1')
+#        elsif exception.response_code == '404'
+#          $stderr.puts "Page not found: #{uri}"
+#          next
         else
           raise exception # Some other error, re-raise
         end
@@ -114,7 +168,7 @@ links.each do |link|
       end
 
       # Replace unwanted characters from the filename
-      filename = filename.gsub(":","").gsub("_","").gsub("/","_")
+      filename = filename.gsub(":","").gsub("_","").gsub("/","_").gsub('?', '').gsub("'", '').gsub('\\', '')
 
       if File.exists?(filename)
         p "Skipping #{filename} as it already exists"
